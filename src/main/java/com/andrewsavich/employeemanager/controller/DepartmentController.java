fa84287d576693ca76a78d6b98299eb30962f4e4
andrew-savich <savichandrew@Gmail.com>
2021-11-10 15:05:42 +0300

    Created DepartmentController

---
+++ src/main/java/com/andrewsavich/employeemanager/controller/DepartmentController.java
@@ -0,0 +1,45 @@
+package com.andrewsavich.employeemanager.controller;
+
+import com.andrewsavich.employeemanager.model.department.Department;
+import com.andrewsavich.employeemanager.service.department.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/department")
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Department>> getDepartmentsList(){
+        List<Department> employees = departmentService.getAllDepartments();
+
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable Long id){
+        Department department = departmentService.getDepartmentById(id);
+
+        return ResponseEntity.ok(department);
+    }
+
+    @PostMapping("/create")
+    public void createDepartment(@RequestBody Department department){
+        departmentService.createDepartment(department);
+    }
+
+    @PutMapping("/update")
+    public void updateDepartment(@RequestBody Department department){
+        departmentService.updateDepartment(department);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteDepartmentById(@PathVariable Long id){
+        departmentService.deleteDepartmentById(id);
+    }
+}
