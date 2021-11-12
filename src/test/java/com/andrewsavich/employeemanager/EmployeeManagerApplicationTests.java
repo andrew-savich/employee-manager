892368126800607442a354fd6cb2dd0b2f80ab0c
andrew-savich <savichandrew@Gmail.com>
2021-11-12 13:26:52 +0300

    Tests of load context

--- src/test/java/com/andrewsavich/employeemanager/EmployeeManagerApplicationTests.java
+++ src/test/java/com/andrewsavich/employeemanager/EmployeeManagerApplicationTests.java
@@ -1,13 +1,28 @@
 package com.andrewsavich.employeemanager;
 
+import com.andrewsavich.employeemanager.controller.EmployeeController;
+import com.andrewsavich.employeemanager.repository.EmployeeRepository;
+import com.andrewsavich.employeemanager.service.EmployeeService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeManagerApplicationTests {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeService employeeService;
+	@Autowired
+	private EmployeeController employeeController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(employeeRepository).isNotNull();
+		Assertions.assertThat(employeeService).isNotNull();
+		Assertions.assertThat(employeeController).isNotNull();
 	}
 
 }
