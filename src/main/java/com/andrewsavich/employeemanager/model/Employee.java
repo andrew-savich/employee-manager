b277d526c5b9ecb55d9208850853b4baf0345b2d
andrew-savich <savichandrew@Gmail.com>
2021-11-12 23:07:58 +0300

    Lombok annotated by @Builder

--- src/main/java/com/andrewsavich/employeemanager/model/Employee.java
+++ src/main/java/com/andrewsavich/employeemanager/model/Employee.java
@@ -1,6 +1,8 @@
 package com.andrewsavich.employeemanager.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -8,6 +10,7 @@ import java.time.LocalDate;
 
 @Data
 @Table("employees")
+@Builder
 public class Employee {
     @Id
     private Long id;
